d752dc5d8478e3c059c1b6f9684bbede6c704909
lulan102 <lulan102@uni-duesseldorf.de>
2016-06-28 20:25:13 +0200

    Added toTextArea function which sets the Text of a given JavaFX TextArea to the code

--- src/CodeManager/CodeManager.java
+++ src/CodeManager/CodeManager.java
@@ -1,10 +1,22 @@
 package CodeManager;
 
+import java.awt.*;
 import java.util.List;
 
 public class CodeManager {
     private List<String> code;
     private List<String> original;
 
-    
+    public CodeManager(List<String> code){
+        this.code = code;
+        original = code;
+    }
+
+    public void toTextArea(TextArea text){
+        String content = "";
+        for(String s : original){
+            content+=s+"\n";
+        }
+        text.setText(content);
+    }
 }
