7754016a6398f29bf5a0428dcc13fbc476733d6e
lulan102 <lulan102@uni-duesseldorf.de>
2016-06-29 20:37:53 +0200

    Changing code from stringlist to string since kata uses string to compile

--- src/main/CodeManager/Code.java
+++ src/main/CodeManager/Code.java
@@ -4,19 +4,15 @@ import java.awt.*;
 import java.util.List;
 
 public class Code {
-    List<String> code;
-    List<String> original;
-    public Code(List<String> code){
+    String code;
+    String original;
+    public Code(String code){
         this.code = code;
         original = code;
     }
 
     public void toTextArea(TextArea text){
-        String content = "";
-        for(String s : code){
-            content+=s+"\n";
-        }
-        text.setText(content);
+        text.setText(code);
     }
 
     public void nextStep(){
