bc538b76cc341d9c90e6a1cd10c80a53c71d7bf6
jan.steimann@hhu.de <jan.steimann@hhu.de>
2016-06-30 19:14:58 +0200

    Methode zum einlesen der xml tags Aufgabenname, aufgabenstellung, klasse eingefügt

--- src/main/XMLManager.java
+++ src/main/XMLManager.java
@@ -1,16 +1,49 @@
-import javax.swing.text.Document;
+import jdk.internal.org.xml.sax.XMLReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 /**
  *Methoden von https://www.youtube.com/watch?v=8MJJ7MWX8Qs
  */
 public class XMLManager {
 
-    public XMLManager(){
+    public static void main(String[] args) throws ParserConfigurationException {
         DocumentBuilderFactory aufgabeXML = DocumentBuilderFactory.newInstance();
-        DocumentBuilder XMLLesen = aufgabeXML.newDocumentBuilder();
-        Document document = XMLLesen.parse();
+
+        try {
+            DocumentBuilder XMLLesen = aufgabeXML.newDocumentBuilder();
+            Document document = XMLLesen.parse(XMLReader.class.getResourceAsStream("/XMLAufgabe.xml"));
+
+            NodeList rootNodes = document.getElementsByTagName("aufgabe");
+            Node aufgabe = rootNodes.item(0);
+            Element noteElement = (Element) aufgabe;
+
+            Node klassenname = noteElement.getElementsByTagName("klassenname").item(0);
+            Element klassennameElemet = (Element) klassenname;
+
+            Node aufgabenstellung = noteElement.getElementsByTagName("aufgabenstellung").item(0);
+            Element aufgabenstellungElement = (Element) aufgabenstellung;
+
+            Node klasse = noteElement.getElementsByTagName("klasse").item(0);
+            Element klasseElement = (Element) klasse;
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (SAXException e){
+            e.printStackTrace();
+        }
+
+
     }
 
 }
