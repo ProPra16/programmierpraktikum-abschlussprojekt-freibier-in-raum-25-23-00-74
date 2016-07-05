6054b0eae4bcb5da450a4c7dcf1149507bd85bc0
lulan102 <lulan102@uni-duesseldorf.de>
2016-07-05 21:00:14 +0200

    Reworked CodeManager. Added backup hashmap for original status, also added seperate hashmap for tests. Added methods for adding, getting, updating and resetting code/test.

--- src/main/CodeManager.java
+++ src/main/CodeManager.java
@@ -1,90 +1,47 @@
-//import java.util.HashMap;
-//
-//public class CodeManager {
-//    private HashMap<String, Code> code;
-//
-//    public CodeManager(){
-//        code = new HashMap<>();
-//    }
-//
-//    public void addCode(Code c){
-//        code.put("Code",c);
-//    }
-//
-//    public void addTest(Code c){
-//        code.put("Test",c);
-//    }
-//
-//    private boolean passAllTests(){
-//        boolean pass = false;
-//        for(String key : code.keySet()){
-//            if(key.equalsIgnoreCase("test")) {
-//                Code value = code.get(key);
-//                pass = value.runable();
-//            }
-//        }
-//        return pass;
-//    }
-//
-//    private boolean oneTestfails(){
-//        int count = 0;
-//        for(String key : code.keySet()){
-//            if(key.equalsIgnoreCase("test")) {
-//                Code value = code.get(key);
-//                if(!value.runable()) count++;
-//            }
-//        }
-//        return count==1;
-//    }
-//
-//    private boolean codeCompiles(){
-//        boolean compiles = false;
-//        for(String key : code.keySet()){
-//            if(key.equalsIgnoreCase("code")) {
-//                Code value = code.get(key);
-//                compiles = value.compileable();
-//            }
-//        }
-//        return compiles;
-//    }
-//
-//    public void fromRedToGreen(){
-//        if(!oneTestfails()||!codeCompiles()){
-//            for(String key : code.keySet()){
-//                if(key.equalsIgnoreCase("test")) {
-//                    Code value = code.get(key);
-//                    value.nextStep();
-//                }
-//            }
-//        }
-//    }
-//
-//    public void backToRed(){
-//        for(String key : code.keySet()){
-//            if(key.equalsIgnoreCase("code")) {
-//                Code value = code.get(key);
-//                value.reset();
-//            }
-//        }
-//    }
-//
-//    public void fromGreenToRefactor(){
-//        if(passAllTests()&&codeCompiles()){
-//            for(String key : code.keySet()){
-//                if(key.equalsIgnoreCase("code")) {
-//                    Code value = code.get(key);
-//                    value.nextStep();
-//                }
-//            }
-//        }
-//    }
-//
-//    public void fromRefactorToRed(){
-//        if(passAllTests()&&codeCompiles()){
-//            for(String key : code.keySet()){
-//                    Code value = code.get(key);
-//                    value.nextStep();
-//            }
-//        }
-//    }
-//}
+import java.util.HashMap;
+
+public class CodeManager {
+    private HashMap<String, Code> code;
+    private HashMap<String, Code> codeOriginal;
+    private HashMap<String, Code> test;
+    private HashMap<String, Code> testOriginal;
+
+    public CodeManager(){
+        code = new HashMap<>();
+        codeOriginal = new HashMap<>();
+        test = new HashMap<>();
+        testOriginal = new HashMap<>();
+    }
+
+    public void addCode(Code c, String name){
+        code.put(name,c);
+    }
+
+    public void addTest(Code c, String name){
+        test.put(name,c);
+    }
+
+    public Code getCode(String name){
+        return code.get(name);
+    }
+
+    public Code getTest(String name){
+        return test.get(name);
+    }
+
+    public void updateCode(String name){
+        codeOriginal.put(name,code.get(name));
+    }
+
+    public void updateTest(String name){
+        testOriginal.put(name,test.get(name));
+    }
+
+    public void resetCode(String name){
+        code.put(name,codeOriginal.get(name));
+    }
+
+    public void resetTest(String name){
+        test.put(name,testOriginal.get(name));
+    }
+}
