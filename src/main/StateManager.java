9671f741e3de2c5bd3fc32e63dae1da4136aecdf
lulan102 <lulan102@uni-duesseldorf.de>
2016-07-06 18:22:59 +0200

    Added Method to move from Red to Green phase. It checks whether the conditions are met and updates code/tests.

--- src/main/StateManager.java
+++ src/main/StateManager.java
@@ -1,17 +1,65 @@
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class StateManager {
     CodeManager cm;
     InterfaceManager im;
+    List<String> codenames;
+    List<String> testnames;
 
     public StateManager(Collection<Code> codes, InterfaceManager im){
+        codenames = new ArrayList<>();
+        testnames = new ArrayList<>();
         cm = new CodeManager();
         this.im = im;
         for(Code c : codes){
             cm.addCode(c, c.getDateiname());
+            codenames.add(c.getDateiname());
         }
         cm.addTest("","first Test");
+        testnames.add("first Test");
+        printToGUI();
     }
 
+    public void printToGUI(){
+        for(String s : codenames){
+            im.setCode(s,cm.getCode(s).getKlasse());
+        }
+        for(String s : testnames){
+            im.setTestCode(s,cm.getTest(s));
+        }
+    }
 
+    public void redToGreen(){
+        boolean codeCompiles=true;
+        //Check if any code has compileErrors
+        for(String s : codenames){
+            CompileManager compiler = new CompileManager(s,cm.getCode(s).getKlasse(),false);
+            codeCompiles=compiler.IncludeCompileErrors();
+        }
+        //Check if exactly 1 test fails
+        int count = 0;
+        for(String s : testnames){
+            CompileManager compiler = new CompileManager(s,cm.getTest(s),true);
+            count+=compiler.returnFailedTestsnumber();
+        }
+        boolean oneFailedTest = count == 1;
+        if(!codeCompiles||oneFailedTest){
+            //Update Code
+            for(String s : codenames){
+                Code c = new Code();
+                c.setDateiname(s);
+                c.setAufgabenstellung(cm.getCode(s).getAufgabenstellung());
+                c.setKlasse(im.getCode(s));
+                cm.addCode(c,s);
+            }
+            //Update Tests
+            for(String s : testnames){
+                cm.addTest(im.getTestCode(s),s);
+            }
+        }
+    }
+
+    
 }
