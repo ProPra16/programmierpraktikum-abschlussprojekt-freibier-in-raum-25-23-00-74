c92342262854cb77a7b34b3a241c8cf32d285b45
jan.steimann@hhu.de <jan.steimann@hhu.de>
2016-07-08 19:05:00 +0200

    FileManager um Methode zum Speichern in xml Datei

--- src/main/FileManager.java
+++ src/main/FileManager.java
@@ -1,6 +1,19 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.Format;
 import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 public class FileManager {
@@ -15,4 +28,34 @@ public class FileManager {
         }
         return aufgaben;
     }
-}
+
+    //Methoden von http://www.drdobbs.com/jvm/creating-and-modifying-xml-in-java/240150782
+        public static void ändereNodeValue(String tagName, String value, String dateiname) throws ParserConfigurationException {
+            DocumentBuilderFactory aufgabeXML = DocumentBuilderFactory.newInstance();
+            try{
+                DocumentBuilder XMLLesen = aufgabeXML.newDocumentBuilder();
+                Document document = XMLLesen.parse(new File("src/main/"+dateiname+".xml"));
+                NodeList rootNodes = document.getElementsByTagName("aufgabe");
+                Node root = rootNodes.item(0);
+                if ( root == null )
+                    return;
+
+                //Findet den Node und ändert den Value
+                for (int y = 0; y < rootNodes.getLength(); y++ ) {
+                    Node data = rootNodes.item(y);
+                    if ( data.getNodeType() == Node.TEXT_NODE ) {
+                        data.setNodeValue(value);
+                        return;
+                    }
+                }
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            catch (SAXException e){
+                e.printStackTrace();
+            }
+
+
+        }
+    }
---
+++ src/test/FileManagerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jan on 08.07.16.
+ */
+public class FileManagerTest {
+    @Test
+    public void setNodeValue() throws Exception {
+
+    }
+
+}
