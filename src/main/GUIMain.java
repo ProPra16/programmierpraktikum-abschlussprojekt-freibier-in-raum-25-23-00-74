75c7b9725ded665d149b85e6dad0fdb90a5e6cd5
bayan100 <yannick.barthel@hhu.de>
2016-07-12 23:27:04 +0200

    futher GUI improvment. Added phase view and working on menu functionality

--- src/main/GUIMain.java
+++ src/main/GUIMain.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import  javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.event.*;
@@ -19,21 +22,34 @@ import java.util.HashMap;
 
 public class GUIMain extends Application
 {
-     // Logik //
-     InterfaceManager interfaceManager;
-     StateManager stateManager;
-     //------
+    // Logik //
+    InterfaceManager interfaceManager;
+    StateManager stateManager;
+    //------
 
-     // GUI //
+    // GUI //
 	Parent root;
 
 	// Aus der fxml-Datei gefiltert
-	 TabPane CodeTabPane;
-     TabPane TestTabPane;
-     TextArea Console;
-     Button DoneButton;
-     Button BackButton;
-     //----------
+	TabPane CodeTabPane;
+    TabPane TestTabPane;
+    TextArea Console;
+    Button DoneButton;
+    Button BackButton;
+    ImageView PhaseView;
+
+    // Menu
+    MenuBar MainMenu;
+    Menu FileMenu;
+    MenuItem NewMButton;
+    MenuItem OpenMButton;
+    MenuItem SaveMButton;
+    MenuItem SaveAsMButton;
+    MenuItem CloseMButton;
+    //----------
+
+    // Phasen-Bilder
+    Image _CodePhase, _RefPhase, _TestPhase;
 
 	 public static void main(String[] args)
 	 {
@@ -44,6 +60,9 @@ public class GUIMain extends Application
 	 @Override
      public void start(Stage stage) throws Exception
      {
+         // Configurations
+         Platform.setImplicitExit(true);
+
          // Versuche Scene aus der fxml zu laden
          try
          {
@@ -61,33 +80,43 @@ public class GUIMain extends Application
 
          Scene scene = new Scene(root);
 
+         // Resourcen laden
+
+         File f = new File("src/main/_CodePhase.png");
+         _CodePhase = new Image(f.toURI().toString());
+         f = new File("src/main/_TestPhase.png");
+         _TestPhase = new Image(f.toURI().toString());
+         f = new File("src/main/_RefPhase.png");
+         _RefPhase = new Image(f.toURI().toString());
+
          // GUI-Elemente aus der fxml filtern
          CodeTabPane = (TabPane)scene.lookup("#CodeTabPane");
          TestTabPane = (TabPane)scene.lookup("#TestTabPane");
          Console = (TextArea)scene.lookup("#Console");
          DoneButton = (Button)scene.lookup("#DoneButton");
          BackButton = (Button)scene.lookup("#BackButton");
+         PhaseView = (ImageView)scene.lookup("#PhaseView");
+
+         MainMenu = (MenuBar)scene.lookup("#MenuBar");
+         FileMenu = MainMenu.getMenus().get(0);
+         NewMButton = FileMenu.getItems().get(0);
+         OpenMButton = FileMenu.getItems().get(1);
+         SaveMButton = FileMenu.getItems().get(2);
+         SaveAsMButton = FileMenu.getItems().get(3);
+         CloseMButton = FileMenu.getItems().get(4);
          //--------------------------------
 
          // GUI-Elemente finalisieren
          CodeTabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
          TestTabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
 
-         DoneButton.setOnAction(new EventHandler<ActionEvent>()
-         {
-             @Override
-             public void handle(ActionEvent e) { nextStep(); }
-         });
-
-         BackButton.setOnAction(new EventHandler<ActionEvent>()
-         {
-             @Override
-             public void handle(ActionEvent e) { backStep(); }
-         });
+         // Eventhandler
+         initializeEventHandler();
+         //-------------------------
 
          // Logik initialisieren
          initializeLogic();
-         //------------------------------
+         //--------------------------
 
          // Show Scene
          if(CodeTabPane != null)
@@ -120,12 +149,13 @@ public class GUIMain extends Application
 
          // InterfaceManager //
          HashMap<String, TextArea> Code = new HashMap<String, TextArea>();
+
          // Ersten Tab neu initialisieren
          Tab ctab = CodeTabPane.getTabs().get(0);
          ctab.setText(XMLManager.getAufgabename());
          Code.put(XMLManager.getAufgabename(), (TextArea)ctab.getContent().lookup("#cta0"));
-
          HashMap<String, TextArea> TestCode = new HashMap<String, TextArea>();
+
          // Ersten Tab neu initialisieren
          Tab ttab = TestTabPane.getTabs().get(0);
          ttab.setText(XMLManager.getAufgabename() + " Test");
@@ -140,7 +170,27 @@ public class GUIMain extends Application
          codes.add(firstC);
 
          stateManager = new StateManager(codes, interfaceManager);
+
+         updatePhase();
      }
+
+    void initializeEventHandler()
+    {
+        DoneButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { nextStep(); } });
+
+        BackButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { backStep(); } });
+
+        // Menu //
+        NewMButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { NewMenuHandler(); } });
+
+        OpenMButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { OpenMenuHandler(); } });
+
+        SaveMButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { SaveMenuHandler(); } });
+
+        SaveAsMButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { SaveAsMenuHandler(); } });
+
+        CloseMButton.setOnAction(new EventHandler<ActionEvent>() { @Override public void handle(ActionEvent e) { CloseMenuHandler(); } });
+    }
 
     // Fügt neuen Code/TestCode Tab hinzu
     int TabCount = 1;
@@ -175,6 +225,16 @@ public class GUIMain extends Application
         TabCount++;
     }
 
+    void updatePhase()
+    {
+        switch (stateManager.currentState)
+        {
+            case "Red": PhaseView.setImage(_CodePhase); break;
+            case "Green": PhaseView.setImage(_TestPhase); break;
+            case "Refactor": PhaseView.setImage(_RefPhase); break;
+        }
+    }
+
     // Eventhandler, entfernt Code/TestCode Tab
     void removeTab(String fileName)
     {
@@ -185,5 +245,36 @@ public class GUIMain extends Application
     void nextStep() { stateManager.toNextStep(); }
 
     void backStep() { stateManager.fromGreenToRed(); }
+
+    // Menu-Eventhandler //
+    void NewMenuHandler()
+    {
+        interfaceManager.writeToConsole("AAAAAAAAAAHHH");
+    }
+
+    void OpenMenuHandler()
+    {
+        interfaceManager.writeToConsole("AAAAAAAAAAHHH");
+    }
+
+    void SaveMenuHandler()
+    {
+        interfaceManager.writeToConsole("AAAAAAAAAAHHH");
+    }
+
+    void SaveAsMenuHandler()
+    {
+        interfaceManager.writeToConsole("AAAAAAAAAAHHH");
+    }
+
+    void CloseMenuHandler()
+    {
+        // Versuche Anwendung zu beenden
+        try {
+            Platform.exit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
