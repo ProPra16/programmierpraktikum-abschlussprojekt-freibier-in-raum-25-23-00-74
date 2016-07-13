3b2da087495958a61c7b6a3a38257f63626ffd12
Brookii <martin.boeckmann@gmx.de>
2016-07-13 16:06:32 +0200

    nächste Version mit Changelistener

--- src/main/Timemanager.java
+++ src/main/Timemanager.java
@@ -1,32 +1,55 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableIntegerValue;
+import javafx.beans.value.ObservableValue;
+
 /**
  * Created by Martin on 13.07.2016.
  */
 public class Timemanager {
-    private int CurrentTime;
+    private IntegerProperty CurrentTime = new SimpleIntegerProperty();
     private int StartTime;
 
     public Timemanager(){
 
     }
 
+    public void createListener(){
+       ChangeListener listener = new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            // Hier muss eingefügt werden, in welchem Feld etwas verändert werden soll
+                System.out.println("Listener hört dir zu");
+            }
+        };
+        CurrentTime.addListener(listener);
+    }
+
     public void setStartTime(int TimeValue){
         StartTime = TimeValue;
-        CurrentTime = StartTime;
+        CurrentTime.setValue(StartTime);
+        createListener();
     }
 
     public int getCurrentTime(){
-        return CurrentTime;
+        return CurrentTime.getValue();
     }
 
     public int getStartTime(){
         return StartTime;
     }
 
+    public void runonestep(){
+        // Ausgabe unterhalb dient zu weiteren Testzwecken
+        // System.out.println(CurrentTime.intValue());
+        CurrentTime.setValue(CurrentTime.getValue() - 1);
+    }
+
     public void runtime() throws InterruptedException {
-        while(CurrentTime >= 0){
-            System.out.println(CurrentTime);
+        while(CurrentTime.getValue() >= 0){
             Thread.sleep(1000);
-            CurrentTime--;
+            runonestep();
         }
     }
 
