f6a1374904a3bec110781bc75abff44b9adb5fea
lulan102 <lulan102@uni-duesseldorf.de>
2016-07-15 04:05:21 +0200

    Import changes in guimain to satisfy travis

--- src/main/GUIMain.java
+++ src/main/GUIMain.java
@@ -1,8 +1,8 @@
 import javafx.application.Application;
 import  javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.FileChooser;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
